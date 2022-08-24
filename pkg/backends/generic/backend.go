c0a737c0d24f3c5df11a9037a9907c170f87e913
Stepan Vyterna <stepan.vyterna@embark-studios.com>
2022-08-24 12:18:03 +0200

    Add generic load-test tool support. This tool is a all-encapsulated docker image with testfile

---
+++ pkg/backends/generic/backend.go
@@ -0,0 +1,202 @@
+package generic
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"go.uber.org/zap"
+	coreV1 "k8s.io/api/core/v1"
+	k8sAPIErrors "k8s.io/apimachinery/pkg/api/errors"
+	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+
+	"github.com/hellofresh/kangal/pkg/backends"
+	loadTestV1 "github.com/hellofresh/kangal/pkg/kubernetes/apis/loadtest/v1"
+)
+
+var (
+	// ErrRequireMinOneDistributedPod Backend spec requires 1 or more DistributedPods
+	ErrRequireMinOneDistributedPod = errors.New("LoadTest must specify 1 or more DistributedPods")
+
+	//ErrRequiredImageDetails Backed spec requires ImageDetails to be specified
+	ErrRequiredImageDetails = errors.New("LoadTest must specify ImageDetails")
+
+	// ErrRequireTestFile the TestFile filed is required to not be an empty string
+	ErrRequireTestFile = errors.New("LoadTest TestFile is required")
+)
+
+func init() {
+	backends.Register(&Backend{})
+}
+
+// Backend is the Generic implementation of backend interface
+type Backend struct {
+	logger         *zap.Logger
+	kubeClientSet  kubernetes.Interface
+	config         *Config
+	podAnnotations map[string]string
+	podTolerations []coreV1.Toleration
+	nodeSelector   map[string]string
+
+	// defined on SetDefaults
+	//masterResources backends.Resources
+	workerResources backends.Resources
+}
+
+// Type returns backend type name
+func (*Backend) Type() loadTestV1.LoadTestType {
+	return loadTestV1.LoadTestTypeGeneric
+}
+
+// GetEnvConfig must return config struct pointer
+func (b *Backend) GetEnvConfig() interface{} {
+	b.config = &Config{}
+	return b.config
+}
+
+// SetDefaults must set default values
+func (b *Backend) SetDefaults() {
+	b.workerResources = backends.Resources{
+		CPULimits:      b.config.WorkerCPULimits,
+		CPURequests:    b.config.WorkerCPURequests,
+		MemoryLimits:   b.config.WorkerMemoryLimits,
+		MemoryRequests: b.config.WorkerMemoryRequests,
+	}
+}
+
+// SetPodAnnotations receives a copy of pod annotations
+func (b *Backend) SetPodAnnotations(podAnnotations map[string]string) {
+	b.podAnnotations = podAnnotations
+}
+
+// SetPodTolerations receives a copy of pod tolerations
+func (b *Backend) SetPodTolerations(tolerations []coreV1.Toleration) {
+	b.podTolerations = tolerations
+}
+
+// SetKubeClientSet receives a copy of kubeClientSet
+func (b *Backend) SetKubeClientSet(kubeClientSet kubernetes.Interface) {
+	b.kubeClientSet = kubeClientSet
+}
+
+// SetLogger receives a copy of logger
+func (b *Backend) SetLogger(logger *zap.Logger) {
+	b.logger = logger
+}
+
+// SetPodNodeSelector receives a copy of pod node selectors
+func (b *Backend) SetPodNodeSelector(nodeselector map[string]string) {
+	b.nodeSelector = nodeselector
+}
+
+// TransformLoadTestSpec use given spec to validate and return a new one or error
+func (b *Backend) TransformLoadTestSpec(spec *loadTestV1.LoadTestSpec) error {
+	if nil == spec.DistributedPods {
+		return ErrRequireMinOneDistributedPod
+	}
+
+	if *spec.DistributedPods <= int32(0) {
+		return ErrRequireMinOneDistributedPod
+	}
+
+	if spec.TestFile == "" {
+		return ErrRequireTestFile
+	}
+
+	if spec.WorkerConfig == "" {
+		return ErrRequiredImageDetails
+	}
+
+	return nil
+}
+
+// Sync check if Backend kubernetes resources have been create, if they have not been create them
+func (b *Backend) Sync(ctx context.Context, loadTest loadTestV1.LoadTest, reportURL string) error {
+	workerJobs, err := b.kubeClientSet.
+		BatchV1().
+		Jobs(loadTest.Status.Namespace).
+		List(ctx, metaV1.ListOptions{
+			FieldSelector: fmt.Sprintf("metadata.name=%s", newWorkerJobName(loadTest)),
+		})
+	if err != nil {
+		b.logger.Error("Error on listing jobs", zap.Error(err))
+		return err
+	}
+
+	if len(workerJobs.Items) > 0 {
+		return nil
+	}
+
+	configMap := newConfigMap(loadTest)
+	_, err = b.kubeClientSet.
+		CoreV1().
+		ConfigMaps(loadTest.Status.Namespace).
+		Create(ctx, configMap, metaV1.CreateOptions{})
+	if err != nil && !k8sAPIErrors.IsAlreadyExists(err) {
+		b.logger.Error("Error on creating testfile configmap", zap.Error(err))
+		return err
+	}
+
+	var secret *coreV1.Secret
+
+	if loadTest.Spec.EnvVars != nil {
+		secret = newSecret(loadTest, loadTest.Spec.EnvVars)
+		_, err = b.kubeClientSet.
+			CoreV1().
+			Secrets(loadTest.Status.Namespace).
+			Create(ctx, secret, metaV1.CreateOptions{})
+		if err != nil && !k8sAPIErrors.IsAlreadyExists(err) {
+			b.logger.Error("Error on creating secret", zap.Error(err))
+			return err
+		}
+	}
+
+	workerJob := newWorkerJob(loadTest, configMap, secret, nil, b.workerResources, b.podAnnotations, b.nodeSelector, b.podTolerations, loadTest.Spec.WorkerConfig, b.logger)
+	_, err = b.kubeClientSet.
+		BatchV1().
+		Jobs(loadTest.Status.Namespace).
+		Create(ctx, workerJob, metaV1.CreateOptions{})
+	if err != nil && !k8sAPIErrors.IsAlreadyExists(err) {
+		b.logger.Error("Error on creating worker job", zap.Error(err))
+		return err
+	}
+
+	return nil
+}
+
+// SyncStatus check the Backend resources and calculate the current status of the LoadTest from them
+func (b *Backend) SyncStatus(ctx context.Context, loadTest loadTestV1.LoadTest, loadTestStatus *loadTestV1.LoadTestStatus) error {
+	if loadTestStatus.Phase == "" {
+		loadTestStatus.Phase = loadTestV1.LoadTestCreating
+	}
+
+	if loadTestStatus.Phase == loadTestV1.LoadTestErrored {
+		return nil
+	}
+
+	_, err := b.kubeClientSet.
+		CoreV1().
+		ConfigMaps(loadTestStatus.Namespace).
+		Get(ctx, newConfigMapName(loadTest), metaV1.GetOptions{})
+	if err != nil {
+		if k8sAPIErrors.IsNotFound(err) {
+			loadTestStatus.Phase = loadTestV1.LoadTestFinished
+			return nil
+		}
+		return err
+	}
+
+	workerJob, err := b.kubeClientSet.
+		BatchV1().
+		Jobs(loadTestStatus.Namespace).
+		Get(ctx, newWorkerJobName(loadTest), metaV1.GetOptions{})
+	if err != nil {
+		return err
+	}
+
+	loadTestStatus.Phase = determineLoadTestPhaseFromJobs(workerJob)
+	loadTestStatus.JobStatus = determineLoadTestStatusFromJobs(workerJob)
+
+	return nil
+}
---
+++ pkg/backends/generic/resources.go
@@ -0,0 +1,190 @@
+package generic
+
+import (
+	"fmt"
+
+	"go.uber.org/zap"
+	batchV1 "k8s.io/api/batch/v1"
+	coreV1 "k8s.io/api/core/v1"
+	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"github.com/hellofresh/kangal/pkg/backends"
+	loadTestV1 "github.com/hellofresh/kangal/pkg/kubernetes/apis/loadtest/v1"
+)
+
+var (
+	loadTestLabelKey         = "app"
+	loadTestMasterLabelValue = "loadtest-master"
+	loadTestWorkerLabelValue = "loadtest-worker-pod"
+)
+
+func newConfigMapName(loadTest loadTestV1.LoadTest) string {
+	return fmt.Sprintf("%s-testfile", loadTest.ObjectMeta.Name)
+}
+
+func newConfigMap(loadTest loadTestV1.LoadTest) *coreV1.ConfigMap {
+	name := newConfigMapName(loadTest)
+
+	ownerRef := metaV1.NewControllerRef(&loadTest, loadTestV1.SchemeGroupVersion.WithKind("LoadTest"))
+
+	return &coreV1.ConfigMap{
+		ObjectMeta: metaV1.ObjectMeta{
+			Name:            name,
+			Namespace:       loadTest.Status.Namespace,
+			OwnerReferences: []metaV1.OwnerReference{*ownerRef},
+		},
+		Data: map[string]string{
+			"testFile": loadTest.Spec.TestFile,
+		},
+	}
+}
+
+func newSecretName(loadTest loadTestV1.LoadTest) string {
+	return fmt.Sprintf("%s-envvar", loadTest.ObjectMeta.Name)
+}
+
+func newSecret(loadTest loadTestV1.LoadTest, envs map[string]string) *coreV1.Secret {
+	name := newSecretName(loadTest)
+
+	ownerRef := metaV1.NewControllerRef(&loadTest, loadTestV1.SchemeGroupVersion.WithKind("LoadTest"))
+
+	return &coreV1.Secret{
+		ObjectMeta: metaV1.ObjectMeta{
+			Name: name,
+			Labels: map[string]string{
+				loadTestLabelKey: name,
+			},
+			OwnerReferences: []metaV1.OwnerReference{*ownerRef},
+		},
+		StringData: envs,
+	}
+}
+
+func newWorkerJobName(loadTest loadTestV1.LoadTest) string {
+	return fmt.Sprintf("%s-worker", loadTest.ObjectMeta.Name)
+}
+
+func newWorkerJob(
+	loadTest loadTestV1.LoadTest,
+	testfileConfigMap *coreV1.ConfigMap,
+	envvarSecret *coreV1.Secret,
+	masterService *coreV1.Service,
+	workerResources backends.Resources,
+	podAnnotations map[string]string,
+	nodeSelector map[string]string,
+	podTolerations []coreV1.Toleration,
+	image loadTestV1.ImageDetails,
+	logger *zap.Logger,
+) *batchV1.Job {
+	name := newWorkerJobName(loadTest)
+
+	ownerRef := metaV1.NewControllerRef(&loadTest, loadTestV1.SchemeGroupVersion.WithKind("LoadTest"))
+
+	envVars := []coreV1.EnvVar{
+		{Name: "GENERIC_TESTFILE", Value: "/data/testfile.json"},
+	}
+
+	envFrom := make([]coreV1.EnvFromSource, 0)
+	if envvarSecret != nil {
+		envFrom = append(envFrom, coreV1.EnvFromSource{
+			SecretRef: &coreV1.SecretEnvSource{
+				LocalObjectReference: coreV1.LocalObjectReference{
+					Name: newSecretName(loadTest),
+				},
+			},
+		})
+	}
+
+	// No support for recovering after a failure
+	backoffLimit := int32(0)
+
+	return &batchV1.Job{
+		ObjectMeta: metaV1.ObjectMeta{
+			Name:      name,
+			Namespace: loadTest.Status.Namespace,
+			Labels: map[string]string{
+				loadTestLabelKey: name,
+			},
+			OwnerReferences: []metaV1.OwnerReference{*ownerRef},
+		},
+		Spec: batchV1.JobSpec{
+			Parallelism:  loadTest.Spec.DistributedPods,
+			Completions:  loadTest.Spec.DistributedPods,
+			BackoffLimit: &backoffLimit,
+			Template: coreV1.PodTemplateSpec{
+				ObjectMeta: metaV1.ObjectMeta{
+					Labels: map[string]string{
+						loadTestLabelKey: loadTestWorkerLabelValue,
+					},
+					Annotations: podAnnotations,
+				},
+				Spec: coreV1.PodSpec{
+					NodeSelector:  nodeSelector,
+					Tolerations:   podTolerations,
+					RestartPolicy: "Never",
+					Containers: []coreV1.Container{
+						{
+							Name:            "generic",
+							Image:           string(image),
+							ImagePullPolicy: "Always",
+							Env:             envVars,
+							VolumeMounts: []coreV1.VolumeMount{
+								{
+									Name:      "testfile",
+									MountPath: "/data/testfile.json",
+									SubPath:   "testfile.json",
+								},
+							},
+							Resources: backends.BuildResourceRequirements(workerResources),
+							EnvFrom:   envFrom,
+						},
+					},
+					Volumes: []coreV1.Volume{
+						{
+							Name: "testfile",
+							VolumeSource: coreV1.VolumeSource{
+								ConfigMap: &coreV1.ConfigMapVolumeSource{
+									LocalObjectReference: coreV1.LocalObjectReference{
+										Name: testfileConfigMap.GetName(),
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+// determineLoadTestPhaseFromJobs reads existing job statuses and determines what the loadtest status should be
+func determineLoadTestPhaseFromJobs(jobs ...*batchV1.Job) loadTestV1.LoadTestPhase {
+	for _, job := range jobs {
+		if job.Status.Failed > int32(0) {
+			return loadTestV1.LoadTestErrored
+		}
+		if job.Status.Active > int32(0) {
+			return loadTestV1.LoadTestRunning
+		}
+		if job.Status.Succeeded == 0 && job.Status.Failed == 0 {
+			return loadTestV1.LoadTestStarting
+		}
+	}
+	return loadTestV1.LoadTestFinished
+}
+
+// determineLoadTestStatusFromJobs reads existing job statuses and determines what the loadtest status should be
+func determineLoadTestStatusFromJobs(jobs ...*batchV1.Job) batchV1.JobStatus {
+	for _, job := range jobs {
+		if job.Status.Failed > int32(0) {
+			return job.Status
+		}
+	}
+	for _, job := range jobs {
+		if job.Status.Active > int32(0) {
+			return job.Status
+		}
+	}
+
+	return jobs[0].Status
+}
